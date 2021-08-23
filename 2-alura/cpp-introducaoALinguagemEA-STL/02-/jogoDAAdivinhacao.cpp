ec227cb9a4436c3d8f2df5aa7ab99f7860e73bb6
jonasht <jhenriquet@outlook.com.br>
2021-08-23 14:52:13 -0300

    novos estudos feitos em aula

---
+++ 2-alura/cpp-introducaoALinguagemEA-STL/02-/jogoDAAdivinhacao.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+
+    cout << "\t***************************************" << endl;
+    cout << "\t*** bem vindo ao jogo de adivinhaca ***" << endl;
+    cout << "\t***************************************" << endl;
+    
+    int numeroSecreto = 42;
+    cout << "o numero secreto eh " << numeroSecreto <<" . nao conte para ninguem" << endl;
+    
+
+}
