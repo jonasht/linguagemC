2d1683d5dcc86be8f600507140f3049af3611ac8
jonasht <jhenriquet@outlook.com.br>
2021-08-23 14:54:27 -0300

    novos estudos feitos em aula

---
+++ 2-alura/cpp-introducaoALinguagemEA-STL/01/jogoDAAdivinhacao.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    // pode ser feito assim
+    // std::cout << "\t***************************************\n";
+    // std::cout << "\t*** bem vindo ao jogo de adivinhaca ***\n";
+    // std::cout << "\t***************************************\n";
+
+    // pode usar endl para poder poder quebrar a linha como \n
+    // std::cout << "\t***************************************" << std::endl;
+    // std::cout << "\t*** bem vindo ao jogo de adivinhaca ***" << std::endl;
+    // std::cout << "\t***************************************" << std::endl;
+
+    // pode tirar std:: quando no topo do arquivo "using namespace std" (as as aspas)
+    cout << "\t***************************************" << endl;
+    cout << "\t*** bem vindo ao jogo de adivinhaca ***" << endl;
+    cout << "\t***************************************" << endl;
+    
+}
